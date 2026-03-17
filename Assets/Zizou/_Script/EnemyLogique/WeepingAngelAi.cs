1f0f8eae12d42130404876c731c33c6ae0c43f7a
Zizou-o-o <zizourabhi76@gmail.com>
2026-03-17 06:11:53 +0100

    Angel AI
    this push is still not compleat but it have the basic function of looking away and it work what left is if there is a wall the angel sill would move

---
+++ Assets/Zizou/_Script/EnemyLogique/WeepingAngelAi.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class WeepingAngelAi : MonoBehaviour
+{
+    [SerializeField] Renderer boundingArea;
+    [SerializeField] LayerMask ignorOneCheck;
+    [SerializeField] Animation anim;
+
+    NavMeshAgent agent;
+    Transform player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = this.gameObject.GetComponent<NavMeshAgent>();
+        player = GameObject.Find("FirstPersonPlayer").transform;
+    }
+    private bool CanMove()
+    {
+        if (boundingArea.isVisible)
+        {
+            return false;
+        }
+        return true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+            if (CanMove())
+            {
+                agent.destination = player.position;
+            }
+            else
+            {
+                agent.destination = this.transform.position;
+            }
+    }
+}
