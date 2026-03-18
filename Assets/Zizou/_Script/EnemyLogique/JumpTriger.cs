4d48f185e1763a5682571489e51b6ce10cf0655a
Zizou-o-o <zizourabhi76@gmail.com>
2026-03-18 10:29:44 +0100

    JumpScaretest
    added a scriped of jumpscar how it works and still some left

---
+++ Assets/Zizou/_Script/EnemyLogique/JumpTriger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public class JumpTriger : MonoBehaviour
+{
+    //public AudioSource scream;
+    public GameObject player;
+    public GameObject jumpCamera;
+    public GameObject flashImg;
+
+    void OnTriggerEnter()
+    {
+        //scream.Play();
+        jumpCamera.SetActive(true);
+        player.SetActive(false);
+        flashImg.SetActive(true);
+        StartCoroutine(EndJump());
+    
+    }
+
+    IEnumerator EndJump()
+    {
+        yield return new WaitForSeconds(2.3f);
+        player.SetActive(true);
+        jumpCamera.SetActive(false);
+        flashImg.SetActive(false);
+
+    }
+
+
+
+}
